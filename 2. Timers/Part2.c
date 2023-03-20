c42814ae6786aed48430484a21babce88df4460a
Ethan Struble <strubl14@students.rowan.edu>
2023-03-20 12:50:23 -0400

    Led Speeds
    Using TB1CCR0 I made the LED have 3 different blinking speeds

--- 2. Timers/Part2.c
+++ 2. Timers/Part2.c
@@ -11,36 +11,34 @@
 
 void gpioInit();
 void timerInit();
-
+int Speed = 0;      //incrementing the speed of LED
 void main(){
 
     WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
-
     gpioInit();
     timerInit();
-
-    // Disable the GPIO power-on default high-impedance mode
-    // to activate previously configured port settings
     PM5CTL0 &= ~LOCKLPM5;
-
     __bis_SR_register(LPM3_bits | GIE);
-
 }
 
 
 void gpioInit(){
-    // @TODO Initialize the Red or Green LED
-
-    // @TODO Initialize Button 2.3
-
+    //1.0 Config(Red LED)
+    P1OUT &= ~BIT0;
+    P1DIR |= BIT0;
+    //2.3 config(button)
+    P2OUT |= BIT3;                          //Configure P2.3 as pulled-up
+    P2REN |= BIT3;                           //P2.3 pull-up register enable
+    P2IES &= ~BIT3;                         //P2.3 Low --> High edge
+    P2IE |= BIT3;                           //P2.3 interrupt enabled
 
 }
-
 void timerInit(){
-    // @TODO Initialize Timer B1 in Continuous Mode using ACLK as the source CLK with Interrupts turned on
 
+    TB1CTL = TBSSEL_1 | MC_2;           //Using ACLK in continuous mode
+    TB1CCR0 = 50000;                    //Setting TimerB1 max to 50000
+    TB1CCTL0 = CCIE;                    //Making the timer an interrupt
 }
-
 
 /*
  * INTERRUPT ROUTINES
@@ -50,18 +48,26 @@ void timerInit(){
 #pragma vector=PORT2_VECTOR
 __interrupt void Port_2(void)
 {
-    // @TODO Remember that when you service the GPIO Interrupt, you need to set the interrupt flag to 0.
-
-    // @TODO When the button is pressed, you can change what the CCR0 Register is for the Timer. You will need to track what speed you should be flashing at.
-
+    P2IFG &= ~BIT3;         //Clearing button for any interrupts
+    Speed +=1;              //Increasing speed by one
 }
-
 
 // Timer B1 interrupt service routine
 #pragma vector = TIMER1_B0_VECTOR
 __interrupt void Timer1_B0_ISR(void)
 {
-    // @TODO You can toggle the LED Pin in this routine and if adjust your count in CCR0.
+    P1OUT ^= BIT0;  //Toggle Red LED
+    if(Speed==1)
+    {
+      TB1CCR0 += 40000;         //CCR0 = Very slow blinking speed
+    }
+    else if(Speed==2)
+    {
+      TB1CCR0 += 4000;        //CCR0 = Mid blinking speed
+    }
+    else if(Speed==3)
+    {
+        TB1CCR0 += 400;      //CCR0 = Fast blinking speed
+        Speed=0;             //Reset Condition
+    }
 }
-
-
